8ee1c7e7954c74d9a8ed1db124bac4d482cd8d25
Alyssa Drobatz <drobatzalyssa@gmail.com>
2017-11-05 17:23:06 -0600

    binary tree complete

--- data.js
+++ data.js
@@ -80,7 +80,6 @@ LinkedList.prototype.forEach = function (iterator) {
 function Alist () {
   // your code here
   this.head = null;
-  this.tail = null;
 }
 
 function AlistNode (key, value, next) {
@@ -92,36 +91,41 @@ function AlistNode (key, value, next) {
 Alist.prototype.set = function (key, value) {
   // your code here
   if (this.head === null){
-    this.head = new AlistNode(key,value);
-    // console.log("null", this.head);
-  } else if (this.head != null){
-    console.log('******')
-    console.log("not null", this.head);
-    var newNode = this.head;
-    this.head = new AlistNode(key,value)
-    newNode.next = this.head
-    console.log('end', this.head)
-
+    this.head = new AlistNode(key, value, null);
+    // console.log(this.head)
+  }
+  if (this.head != null){
+    var node = this.head;
+    this.head = new AlistNode(key, value);
+    this.head.next = node;
   }
 
-    return this; // for chaining; do not edit
+     // for chaining; do not edit
+
+  return this;
   
 };
 
 Alist.prototype.get = function (key) {
   // your code here
-    if(this.head){
-      console.log('%%%%%%%%%%')
-      console.log(this.head)
-      var currentHead = this.head;
-      // console.log(currentHead)
-      this.head = currentHead.next;
-      // console.log(this.head)
-      console.log(currentHead.value)
-      return currentHead.value;
-    }  else {
-      this.head = null;
+  // if(this.head){
+  //   var currentHead = this.head
+  //   this.head = currentHead.next;
+  //   return currentHead.value
+  //   }   
+  if (this.head){
+    for (var word in this.head){
+      if (word === 'next'){
+        if (this.head[word] === null){
+          var currentHead = this.head;
+        this.head = currentHead.next;
+        return currentHead.value;
+        }
+        
+      }      
     }
+  }
+  return this.head.value
 };
 
 
@@ -160,25 +164,110 @@ HashTable.prototype.get = function (key) {
 
 function BinarySearchTree (val) {
   // your code here
+  this.value = val;
+  this.counter = 1;
+  this.left = null;
+  this.right = null;
 }
 
 BinarySearchTree.prototype.insert = function (val) {
   // your code here
+  this.counter++;
+  if (this.value > val && this.left === null){
+    this.left = new BinarySearchTree(val);
+  }
+  else if (this.value > val && this.left != null){
+    this.left.insert(val);
+  }
+  else if (this.value < val && this.right === null){
+    this.right = new BinarySearchTree(val);
+  }
+  else {
+    this.right.insert(val);
+  }
   return this; // for chaining, do not edit
 };
 
 BinarySearchTree.prototype.min = function () {
   // your code here
+  // console.log(this.value)
+  // console.log(this.left.value)
+  // console.log(this.left)
+  if (this.left === null){
+    var newLeft = this.value;
+    //below is returning 5;
+    // console.log(this.value);
+    // console.log(newLeft)
+    //console.log(this.left.value)
+    return newLeft;
+  }
+  if (this.left && this.left != null){
+    //console.log(this.left)
+    var findLeft = this.left.min();
+    return findLeft;
+    // console.log("func", func)
+  }
 };
 
 BinarySearchTree.prototype.max = function () {
   // your code here
+    if (this.right === null){
+    var newright = this.value;
+    //below is returning 5;
+    // console.log(this.value);
+    // console.log(newright)
+    //console.log(this.right.value)
+    return newright;
+  }
+  if (this.right && this.right != null){
+    //console.log(this.right)
+    var findRight = this.right.max();
+    return findRight;
+    // console.log("func", func)
+  }
 };
 
 BinarySearchTree.prototype.contains = function (val) {
   // your code here
+  var bool = true;
+  if (bool){
+    if (this.value === val){
+      bool = true;
+    } else if (this.value < val){
+      if(this.right === null){
+        bool = false;
+      } else {
+        var right = this.right.contains(val);
+        if (right === false){
+          return false
+        }
+      }
+    } else if (this.value > val){
+      if (this.left === null){
+        bool = false;
+      } else {
+        var left = this.left.contains(val);
+        if (left === false){
+          return false;
+        }
+      }
+    } else {
+      bool = false;
+    }
+  }
+  return bool;
+
 };
 
 BinarySearchTree.prototype.traverse = function (iterator) {
   // your code here
+  if (this.left){
+    this.left.traverse(iterator)
+  }
+  iterator(this.value);
+  if(this.right){
+    this.right.traverse(iterator)
+  }
+
+  console.log(iterator)
 };
